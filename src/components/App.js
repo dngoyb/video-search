8f95d938c1cb2d6ad670c22d6bd2f773ad877285
dngoyb <dngoyb@gmail.com>
2021-05-24 21:01:59 +0200

    videothumbnails and description dispalyed

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends Component {
-	state = { videos: [] };
+	state = { videos: [], selectedVideo: null };
 	onTermSubmit = async (term) => {
 		const res = await youtube.get('/search', {
 			q: term,
 		});
 		this.setState({ videos: res.data.items });
 	};
+
+	onVideoSelect = (video) => {
+		this.setState({ selectedVideo: video });
+	};
 	render() {
 		return (
 			<div className='ui container'>
-				<SearchBar onFormSubmit={this.onTermSubmit} />I have{' '}
-				{this.state.videos.length} videos
+				<SearchBar onFormSubmit={this.onTermSubmit} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={this.onVideoSelect}
+					videos={this.state.videos}
+				/>
 			</div>
 		);
 	}
